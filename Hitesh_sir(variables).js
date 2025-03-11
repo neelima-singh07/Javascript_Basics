2f431f5c6f04ba30b81b19939958f87d7fb68437
neelima-singh07 <neelima.arun2007@gmail.com>
2025-03-11 15:00:32 +0530

    Add files via upload

---
+++ Hitesh_sir(variables).js
@@ -0,0 +1,32 @@
+console.log("Neelima Singh")
+const accountId =144553 //change nhi kiya jaa skta 
+let accountEmail="neelima@google.com"
+var accountPw="1234"
+accountCity="Delhi"
+
+//try to change 
+accountPw="new123"
+accountEmail="neelima_arun@google.com"
+console.log(accountPw) 
+console.log(accountEmail)
+console.table([accountEmail, accountPw, accountCity]) //tabular form me dikhega sab 
+/* 
+Prefer not yo use var because of issue in block . use let always
+*/ 
+
+//Data types in javascript 
+"use strict"; //treat all JS code as newer version
+//alert("Hello neelima!");
+console.log(40/8);
+let name="neelima"; //string
+let age=18;//numbe
+let isLoggedIn=false;//boolean
+writer=null ; 
+book=undefined ; //value assign kri ni hai
+//symbol ==>uniqueness ke liye 
+//object
+console.log(typeof "neelima");
+console.log(typeof writer);//is object imp ha learn it 
+console.log(typeof book);//is undefined
+
+
